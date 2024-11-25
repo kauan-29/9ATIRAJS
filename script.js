2e310ca68288612731cc512830770c79164774dd
kauan-29 <kauan.junio.alves@escola.pr.gov.br>
2024-11-25 14:33:52 -0300

    etapa 1

---
+++ script.js
@@ -0,0 +1,4 @@
+// Aqui você pode adicionar interações, se necessário, para os projetos.
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Portfólio carregado com sucesso!");
+});
